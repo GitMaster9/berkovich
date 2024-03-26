a3df0a98c3b4e49cd370a6345e3c5947684b3b35
mfrankic <marin.franki7@gmail.com>
2024-03-26 15:43:09 +0100

    fix

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ def draw_graph(test: BerkovichTest, index: int):
     plt.plot(displacement, load)
     plt.xlabel("Displacement (nm)")
     plt.ylabel("Load (mN)")
-    plt.title(f"Load-Displacement Curve Test {index + 1}")
-    plt.savefig(f"images/test_{index + 1}_load_displacement_curve.png")
+    plt.title(f"Load-Displacement Curve Test {index}")
+    plt.savefig(f"images/test_{index}_load_displacement_curve.png")
     plt.clf()
 
 # Main
@@ -46,7 +46,7 @@ def main():
         data.append(get_sheet(i))
 
     for i in range(36):
-        test = BerkovichTest(data, i)
+        test = BerkovichTest(data[i], i)
 
         draw_graph(test, 36 - i)
         
